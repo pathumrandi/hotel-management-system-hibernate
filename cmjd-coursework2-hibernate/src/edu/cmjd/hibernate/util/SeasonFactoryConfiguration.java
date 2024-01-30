ef4e837f4beed7476b57ca6da7279bd2b28c2bf6
Pathum Palawaththa <pathumpalawaththa1998@gmail.com>
2024-01-30 21:18:21 +0530

    Create Pakages

---
+++ cmjd-coursework2-hibernate/src/edu/cmjd/hibernate/util/SeasonFactoryConfiguration.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.cmjd.hibernate.util;
+
+/**
+ *
+ * @author pathum
+ */
+public class SeasonFactoryConfiguration {
+    
+}
